8b53d099d8430ab7e683c114e6272d53660c0e0f
Pedro Prates <dev@pprates.com>
2026-09-08 14:17:20 +0100

    main: add simple prompt response with token counter

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
 def main():
     print("Hello from ai-agent!")
+
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+
+    if not api_key:
+        raise RuntimeError("API key no loaded")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": args.user_prompt
+            }
+        ],
+    )
+    if not response.usage:
+        raise RuntimeError("Failed API request")
+
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+    print(f"Response: {response.choices[0].message.content}")
 
 
 if __name__ == "__main__":
