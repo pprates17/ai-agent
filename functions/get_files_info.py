985fd55de50bcd8600b3a9396f7bcbb656dad3e1
Pedro Prates <dev@pprates.com>
2026-09-10 16:09:23 +0100

    functions: complete get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -7,12 +7,22 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
         common_path = os.path.commonpath([working_dir_abs, target_dir])
 
-        if not os.path.isdir(directory):
-            return f'Error: "{directory}" is not a directory'
         if not common_path == working_dir_abs:
             return (f'Error: Cannot list "{directory}"'
                     'as it is outside the permitted working directory')
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
 
-        return f'Success: "{directory}" is within the working directory'
+        dir_content: list[str] = os.listdir(target_dir)
+        files_info = ""
+        for path in dir_content:
+            complete_path = target_dir + "/" + path
+            files_info += (
+                f'- {path}: '
+                f'file_size={os.path.getsize(complete_path)} bytes,'
+                f'is_dir={os.path.isdir(complete_path)}\n'
+            )
+
+        return files_info
     except Exception as e:
-        print(f'Error: {e}')
+        print(f'Error listing dir contents: {e}')
